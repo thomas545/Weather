0f5dfc67922e21d6c865806efbe9629cc872e44c
thomas <thomas.adel31@gmail.com>
2019-10-03 16:08:35 +0200

    add weather API

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
+import requests
+import time, uuid
+from urllib.parse import quote, urlencode
+from urllib.request import urlopen, Request
+import hmac, hashlib
+from base64 import b64encode
+import codecs
+import json
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from django.conf import settings
@@ -22,13 +31,56 @@ class WeatherView(generics.ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         # from django.contrib.auth.models import User
         # user = User.objects.get(pk=1)
-        
         user = request.user
-        # TODO weather Api here.
+        # weather Api 
+        city = request.data.get('city')
+        url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss'
+        method = 'GET'
+        app_id = 'pMZPOy50'
+        consumer_key = 'dj0yJmk9Qkdxbm1ZNjVZUXJJJmQ9WVdrOWNFMWFVRTk1TlRBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTJi'
+        consumer_secret = '14e6cf3f85f18098d47bcc929368943730cb0a99'
+        concat = '&'
+        query = {'location': city, 'format': 'json'}
+        oauth = {
+            'oauth_consumer_key': consumer_key,
+            'oauth_nonce': uuid.uuid4().hex,
+            'oauth_signature_method': 'HMAC-SHA1',
+            'oauth_timestamp': str(int(time.time())),
+            'oauth_version': '1.0'
+        }
+        merged_params = query.copy()
+        merged_params.update(oauth)
+        sorted_params = [k + '=' + quote(merged_params[k], safe='') for k in sorted(merged_params.keys())]
+        signature_base_str =  method + concat + quote(url, safe='') + concat + quote(concat.join(sorted_params), safe='')
+
+        composite_key = quote(consumer_secret, safe='') + concat
+        key = codecs.encode(composite_key)
+        msg = codecs.encode(signature_base_str)
+        oauth_signature = b64encode(hmac.new(key, msg, hashlib.sha1).digest())
+
+        oauth['oauth_signature'] = oauth_signature
+        auth_header = 'OAuth ' + ', '.join(['{}="{}"'.format(k,v) for k,v in oauth.items()])
+
+        url = url + '?' + urlencode(query)
+        print(url)
+        r = requests.get(url).status_code
+        if bool(r == 401) or bool(r == 400) or bool(r == 500):
+            response = None
+
+        if bool(r == 200):
+            request = Request(url)
+            request.add_header('Authorization', auth_header)
+            request.add_header('X-Yahoo-App-Id', app_id)
+            response = urlopen(request).read()
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(user=user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        data = {
+            "data": serializer.data,
+            "weather": response
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
 
 
 
